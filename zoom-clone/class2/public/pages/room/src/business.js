377dc19c5d9282dd7c067b4aeccc040e860d347a
EmersonOliveiraDev <emersonhaw@gmail.com>
2023-04-05 04:09:35 -0300

    feat: add-week-02-class-2

---
+++ zoom-clone/class2/public/pages/room/src/business.js
@@ -0,0 +1,71 @@
+class Business {
+  constructor({ room, media, view, socketBuilder, peerBuilder }) {
+    this.room = room;
+    this.media = media;
+    this.view = view;
+
+    this.socketBuilder = socketBuilder;
+    this.peerBuilder = peerBuilder;
+
+    this.socket = {};
+    this.currentPeer = {};
+    this.currentStream = {};
+  }
+
+  static initialize(deps) {
+    const instance = new Business(deps);
+    return instance._init();
+  }
+
+  async _init() {
+    this.currentStream = await this.media.getCamera(); // true
+    this.socket = this.socketBuilder
+      .setOnUserConnected(this.onUserConnected())
+      .setOnUserDisconnected(this.onUserDisconnected())
+      .build();
+
+    this.currentPeer = await this.peerBuilder
+      .setOnError(this.onPeerError())
+      .setOnConnectionOpened(this.onPeerConnectionOpened())
+      .build();
+
+    this.addVideoStream("teste01");
+  }
+
+  addVideoStream(userId, stream = this.currentStream) {
+    const isCurrentId = false;
+    this.view.renderVideo({
+      userId,
+      // muted: false,
+      stream,
+      isCurrentId,
+    });
+  }
+
+  onUserConnected = function () {
+    return (userId) => {
+      console.log("user connected!", userId);
+      
+    };
+  };
+
+  onUserDisconnected = function () {
+    return (userId) => {
+      console.log("user disconnected!", userId);
+    };
+  };
+
+  onPeerError = function () {
+    return (error) => {
+      console.error("error on peer!", error);
+    };
+  };
+
+  onPeerConnectionOpened = function () {
+    return (peer) => {
+      const id = peer.id;
+      console.log(id);
+      this.socket.emit("join-room", this.room, id);
+    };
+  };
+}
--- zoom-clone/class2/public/pages/room/src/view.js
+++ zoom-clone/class2/public/pages/room/src/view.js
@@ -36,8 +36,8 @@ class View {
         videoGrid.append(div)
     }
 
-    renderVideo({userId, stream=null, url=null, isCurrentId=false}) {
-        const video = this.createVideoElement({src: url, srcObject: stream})
+    renderVideo({userId, stream=null, url=null, isCurrentId=false, muted=true}) {
+        const video = this.createVideoElement({muted, src: url, srcObject: stream})
         this.appendToHTMLTree(userId, video, isCurrentId)
     }
 }
