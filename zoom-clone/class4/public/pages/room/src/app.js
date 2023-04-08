c3ca6b180cc9a17653fb5647149c4082356775d4
EmersonOliveiraDev <emersonhaw@gmail.com>
2023-04-08 04:07:36 -0300

    docs: add-week-02-class-4

---
+++ zoom-clone/class4/public/pages/room/src/app.js
@@ -0,0 +1,35 @@
+const onload = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const room = urlParams.get("room");
+  console.log("this is the room", room);
+
+  const socketUrl = "http://localhost:3000";
+  const socketBuilder = new SocketBuilder({ socketUrl });
+
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: "localhost",
+      path: "/",
+    },
+  });
+
+  const peerBuilder = new PeerBuilder({ peerConfig });
+
+  const view = new View();
+  const media = new Media();
+  const deps = { room, media, view, socketBuilder, peerBuilder };
+
+  try {
+    Business.initialize(deps);
+  } catch (err) {
+    console.log(err);
+  }
+
+  // view.renderVideo({userId: 'teste01', url:'https://media.giphy.com/media/qgQUggAC3Pfv687qPC/giphy.mp4',})
+  // view.renderVideo({userId: 'teste02', isCurrentId: true, url:'https://media.giphy.com/media/qgQUggAC3Pfv687qPC/giphy.mp4',})
+  // view.renderVideo({userId: 'teste03', url:'https://media.giphy.com/media/qgQUggAC3Pfv687qPC/giphy.mp4',})
+};
+
+window.onload = onload;
